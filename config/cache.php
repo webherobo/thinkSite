b56f341693d32806a63567948b99c08d0979ac47
webherobo <webherobo@qq.com>
2019-10-08 10:10:27 +0800

    更新

--- config/cache.php
+++ config/cache.php
@@ -7,7 +7,7 @@ use think\facade\Env;
 
 return [
     // 默认缓存驱动
-    'default' => Env::get('cache.driver', 'file'),
+    'default' => Env::get('cache.driver', 'redis'),
 
     // 缓存连接方式配置
     'stores'  => [
@@ -26,5 +26,13 @@ return [
             'serialize'  => [],
         ],
         // 更多的缓存连接
+		//redis配置
+        'redis'  => [
+        'type'      => 'redis',
+        'host'      => '127.0.0.1',
+        'port'      => '6379',//你redis的端口号，可以在配置文件设置其他的
+        'password'  => '', //这里是你redis配置的密码，如果没有则留空
+        'timeout'   => 3600 //缓存时间
+    ],
     ],
 ];
