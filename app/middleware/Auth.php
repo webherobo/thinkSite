4435209d22f666da5c0bddd0a369e7e2c95ef223
webherobo <webherobo@qq.com>
2019-10-14 18:08:21 +0800

    添加中间件

---
+++ app/middleware/Auth.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\middleware;
+
+class Auth
+{
+    /**
+     * 处理请求
+     *
+     * @param Request $request
+     * @param \Closure $next
+     * @return Response
+     */
+    public function handle($request, \Closure $next)
+    {
+        //
+    }
+}
