37ddb3646abe01955df3bcfd744d7321419d9001
boning <webherobo@qq.com>
2019-10-19 15:22:13 +0800

    更新

--- app/controller/ApiTest.php
+++ app/controller/ApiTest.php
@@ -11,28 +11,35 @@ class ApiTest extends ApiBase
 {
 
     //filelock文件锁
-    public function filelock()
+    public function filelock($type)
     {
         $fp = fopen("logs/app.log", "a+");
-
-        if (flock($fp, LOCK_EX)) {  // 进行排它型锁定
-            fwrite($fp, "Write something here\n");
-            fgets($fp);
-            fflush($fp);            // flush output before releasing the lock
-            flock($fp, LOCK_UN);    // 释放锁定
+        if ($type) {
+            if (flock($fp, LOCK_EX)) {  // 进行排它型锁定
+                fwrite($fp, "Write something here\n");
+                fgets($fp);
+                fflush($fp);            // flush output before releasing the lock
+                flock($fp, LOCK_UN);    // 释放锁定
+            } else {
+                echo "文件正在被其他程序占用";
+            }
         } else {
-            echo "文件正在被其他程序占用";
+            //加锁 LOCK_NB
+            if (flock($fp, LOCK_EX | LOCK_NB)) {
+                sleep(10);
+                //事例二 fopen->pcntl_fork()获取同一个打开文件句柄反之p->f则不同
+                $pid = pcntl_fork();
+                if ($pid == 0) {
+                    fwrite($fp, "子进程\n");
+                } else {
+                    fwrite($fp, "父进程\n");
+                }
+                echo "非阻塞获取成功";
+                flock($fp, LOCK_UN);// 解锁
+            } else {
+                echo "非阻塞获取失败";
+            }
         }
-
-        //加锁 LOCK_NB
-        if (flock($fp, LOCK_EX | LOCK_NB)) {
-            sleep(10);
-            echo "阻塞";
-            flock($fp, LOCK_UN);// 解锁
-        } else {
-            echo "非阻塞";
-        }
-
         fclose($fp);
 
     }
--- app/service/LockService.php
+++ app/service/LockService.php
@@ -11,7 +11,7 @@ use think\Service;
  */
 class LockService extends Service
 {
-    private $retryDelay;//重试延迟
+    private $retryDelay;
     private $retryCount;
     private $clockDriftFactor = 0.01;//漂移的因素
     private $quorum;//仲裁
@@ -123,40 +123,38 @@ class LockService extends Service
     }
 
     //filelock文件锁
-    public function filelock()
+    public function filelock($type)
     {
         $fp = fopen("logs/app.log", "a+");
-        //加锁 阻塞
-        if (flock($fp, LOCK_EX)) {  // 进行排它型锁定
-            fwrite($fp, "Write something here\n");
-            fgets($fp);
-            fflush($fp);            // flush output before releasing the lock
-            flock($fp, LOCK_UN);    // 释放锁定
+        if ($type) {
+            if (flock($fp, LOCK_EX)) {  // 进行排它型锁定
+                fwrite($fp, "Write something here\n");
+                fgets($fp);
+                fflush($fp);            // flush output before releasing the lock
+                flock($fp, LOCK_UN);    // 释放锁定
+            } else {
+                echo "文件正在被其他程序占用";
+            }
         } else {
-            echo "文件正在被其他程序占用";
-        }
+            //加锁 LOCK_NB
+            if (flock($fp, LOCK_EX | LOCK_NB)) {
+                sleep(10);
+                //事例二 fopen->pcntl_fork()获取同一个打开文件句柄反之p->f则不同
 
-        //加锁 LOCK_NB 非阻塞
-        if (flock($fp, LOCK_EX | LOCK_NB)) {
-            sleep(10);
-            echo "阻塞";
-            flock($fp, LOCK_UN);// 解锁
-        } else {
-            echo "非阻塞";
+                $pid = pcntl_fork();
+
+                if ($pid == 0) {
+                    fwrite($fp, "子进程\n");
+                } else {
+                    fwrite($fp, "父进程\n");
+                }
+                echo "非阻塞获取成功";
+                flock($fp, LOCK_UN);// 解锁
+            } else {
+                echo "非阻塞获取失败";
+            }
         }
         fclose($fp);
-
-        //事例二 fopen pcntl_fork()获取同一个打开文件句柄反之则不同
-
-        $pid = pcntl_fork();
-
-        $fp = fopen("demo.log", "a");
-
-        if ($pid == 0) {
-            fwrite($fp, "子进程\n");
-        } else {
-            fwrite($fp, "父进程\n");
-        }
 
     }
 
