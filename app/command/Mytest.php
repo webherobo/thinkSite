358fd12c6b311e8d80eb251e5e7393ff2cc201d5
boning <webherobo@qq.com>
2019-10-26 19:02:33 +0800

    更新

--- app/command/Mytest.php
+++ app/command/Mytest.php
@@ -13,14 +13,16 @@ class Mytest extends Command
     protected function configure()
     {
         // 指令配置
-        $this->setName('test')
-            ->setDescription('the test command');
+        $this->setName('hello')
+            ->addArgument('type', Argument::OPTIONAL, "0:消费者 1:生产者")
+            ->addOption('test', null, Option::VALUE_REQUIRED, 'city name')
+            ->setDescription('option=test name=hello argv=type');
     }
 
     protected function execute(Input $input, Output $output)
     {
         // rabbitmq队列任务指令输出
-        $type=$input->hasOption('type');
+        $type=$input->hasOption('test');
         if ($type==0) {
             $mqConf = config('rabbit_mq')["rabbit_mq_queue"]["test"];
             $this->app->rabbitMqService->instance($mqConf);
