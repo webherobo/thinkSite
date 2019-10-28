9c4a78c724ac1cbfeee94c08469f0eabda7bd44c
boning <webherobo@qq.com>
2019-10-28 16:28:18 +0800

    更新

--- app/controller/SwooleBase.php
+++ app/controller/SwooleBase.php
@@ -15,9 +15,33 @@ use think\swoole\concerns\InteractsWithRpc;
 use think\swoole\concerns\InteractsWithServer;
 use think\swoole\concerns\InteractsWithSwooleTable;
 use think\swoole\concerns\InteractsWithWebsocket;
+use swoole_client;
 
 class SwooleBase extends BaseController
 {
+    //客户端swoole
+    public function swoole()
+    {
+
+        $client = new swoole_client(SWOOLE_SOCK_TCP);
+        //连接到服务器
+        if (!$client->connect('127.0.0.1', 8081, 0.5)) {
+            die("connect failed.");
+        }
+        //向服务器发送数据
+        if (!$client->send("hello world")) {
+            echo ‘发送失败‘;
+        }
+        //从服务器接收数据
+        $data = $client->recv();
+        if (!$data) {
+            die("recv failed.");
+        }
+        echo $data;
+        //关闭连接
+        $client->close();
+    }
+
     //swoole特性
     use InteractsWithHttp;
 
