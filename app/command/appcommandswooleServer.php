ed67e5269c2b62cc927406d278a7377e02077041
webherobo <webherobo@qq.com>
2019-10-28 16:32:43 +0800

    更新

---
+++ app/command/appcommandswooleServer.php
@@ -0,0 +1,26 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\command;
+
+use think\console\Command;
+use think\console\Input;
+use think\console\input\Argument;
+use think\console\input\Option;
+use think\console\Output;
+
+class appcommandswooleServer extends Command
+{
+    protected function configure()
+    {
+        // 指令配置
+        $this->setName('swooleServer')
+            ->setDescription('the swooleServer command');        
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+    	// 指令输出
+    	$output->writeln('swooleServer');
+    }
+}
